f1ea9bedf062e16e24ae330df71447b8991446a1
LC-BARBE <loic.cecyl@gmail.com>
2024-06-14 17:11:53 +0200

    add config to docker and dev context

--- dockerBuildRun.sh
+++ dockerBuildRun.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#docker rm -f mspr-kawa-db || true  &
+docker rm -f mspr-kawa-db || true  &
 docker rm -f postgresql || true &
 wait
 
-#docker rmi -f mspr-kawa-db || true &
+docker rmi -f mspr-kawa-db || true &
 docker rmi -f postgresql || true &
 wait
 
-#docker build -t mspr-kawa-db . &
+docker build -t mspr-kawa-db . &
 docker build -t postgresql ./db_main &
 wait
 
@@ -18,8 +18,7 @@ echo "Image mspr-kawa-db - postgresql              | OK |"
 echo ""
 echo ""
 
-#docker run --name mspr-kawa-db -d -p 8077:8077 -e SPRING_PROFILES_ACTIVE=prod mspr-kawa-db
-#docker run --name mspr-kawa-db -d -p 8077:8077 mspr-kawa-db &
+docker run --name mspr-kawa-db -d -p 8077:8077 -e SPRING_PROFILES_ACTIVE=prod mspr-kawa-db &
 docker run --name postgresql -d -p 5432:5432 postgresql &
 wait
 
